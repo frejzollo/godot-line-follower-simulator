b1e592c9c7adfd1a803c3d4b119998247b39715a
Jałokim <robimy_po_swojemu@o2.pl>
2025-04-05 14:16:10 +0200

    Add PID controller script

---
+++ scripts/microcotroller/utils/PID.gd
@@ -0,0 +1,36 @@
+extends Node
+
+class_name PIDController
+
+var kp: float
+var ki: float
+var kd: float
+
+var setpoint: float = 0.0
+var input: float = 0.0
+var output: float = 0.0
+
+var prev_error: float = 0.0
+var integral: float = 0.0
+var last_time: float = 0.0
+
+func _init(kp_: float, ki_: float, kd_: float):
+	kp = kp_
+	ki = ki_
+	kd = kd_
+	last_time = Time.get_ticks_msec() / 1000.0
+
+func compute():
+	var current_time = Time.get_ticks_msec() / 1000.0
+	var delta_time = current_time - last_time
+	if delta_time <= 0:
+		return
+	
+	var error = setpoint - input
+	integral += error * delta_time
+	var derivative = (error - prev_error) / delta_time if delta_time > 0 else 0.0
+	
+	output = (kp * error) + (ki * integral) + (kd * derivative)
+
+	prev_error = error
+	last_time = current_time
