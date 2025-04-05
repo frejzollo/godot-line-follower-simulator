3a54ad0ed05078ccff9ba937679ef7293735210e
Jałokim <robimy_po_swojemu@o2.pl>
2025-04-05 14:11:50 +0200

    Add set_velocity() function to 2d motor

--- scripts/scripts2D/motor.gd
+++ scripts/scripts2D/motor.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 class_name Motor
 
-@export var max_abs_velocity 		=  	100.0
+@export var max_abs_velocity 		=  	255.0
 @export var min_abs_velocity 		= 	10.0
 @export var velocity: Vector2	= Vector2(0.0, 0.0)
 
@@ -13,6 +13,15 @@ func add_velocity(dV: float) -> void:
 	elif velocity.y < -max_abs_velocity:
 		velocity.y = -max_abs_velocity
 
+func set_velocity(new_v: float) -> void:
+	if new_v > max_abs_velocity:
+		velocity.y = max_abs_velocity
+	elif new_v < -max_abs_velocity:
+		velocity.y = -max_abs_velocity
+	else:
+		velocity.y = new_v
+		
+		
 func get_velocity() -> Vector2:
 	if abs(velocity.y) >= min_abs_velocity:
 		return velocity
